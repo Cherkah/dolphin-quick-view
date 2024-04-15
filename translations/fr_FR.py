914217f37d054595a83f2efca721d38bc4457ed9
Cherkah <cherifkahsum@gmail.com>
2024-04-15 02:26:09 +0200

    Add files via upload

---
+++ translations/fr_FR.py
@@ -0,0 +1,14 @@
+dictionary = dict(
+    loading_placeholder="Chargement…",
+    size="Taille",
+    last_modified="Dernière modification",
+    files="Fichier",
+    unknown="Inconnu",
+    container_file_too_big="Visualisation impossible: Le fichier est trop grand.",
+    click_here="Cliquez ici!",
+    libreoffice_preview_unavailable="Visualisation impossible.",
+    libreoffice_timeout="Le chargement du fichier à pris trop de temps.",
+    libreoffice_not_found="Libreoffice n'est pas installé:",
+    libreoffice_file_not_found="Fichier introuvable - Flatpack peut ne pas être configuré correctement:",
+    unsupported_file_type="Type de fichier non-supporté.",
+)
